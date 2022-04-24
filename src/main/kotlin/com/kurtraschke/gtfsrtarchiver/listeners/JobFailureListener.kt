13babb9487996286579771f9d38a568463f1b6a4
Kurt Raschke <kurt@kurtraschke.com>
2022-04-23 22:04:52 -0400

    Clean up and fix job failure handler.

--- src/main/kotlin/com/kurtraschke/gtfsrtarchiver/listeners/JobFailureListener.kt
+++ src/main/kotlin/com/kurtraschke/gtfsrtarchiver/listeners/JobFailureListener.kt
@@ -6,19 +6,33 @@ import org.quartz.*
 import org.quartz.DateBuilder.IntervalUnit.SECOND
 import org.quartz.DateBuilder.futureDate
 import org.quartz.listeners.JobListenerSupport
+import java.time.Duration
+import java.time.Instant
+import kotlin.math.log
 import kotlin.math.pow
+import kotlin.time.Duration.Companion.hours
+import kotlin.time.toKotlinDuration
 
 private const val MAX_CONSECUTIVE_FAILURES = 3
+private const val PAUSE_PERIOD = 30
+private const val PAUSE_ESCALATION = 1.25
+private const val MAX_PAUSE_DURATION = 900
+private val RESET_PAUSE_AFTER = 6.hours
+private val MAX_PAUSE_COUNT = log(MAX_PAUSE_DURATION / PAUSE_PERIOD.toDouble(), PAUSE_ESCALATION)
 
 class JobFailureListener : JobListenerSupport() {
     private var consecutiveFailureCount = 0
     private var pauseCount = 0
+    private var lastFailure: Instant? = null
 
     override fun getName(): String = "JobFailureListener"
 
     override fun jobWasExecuted(context: JobExecutionContext, jobException: JobExecutionException?) {
         val threwException = jobException != null
         val reportedError = (context.result as FeedContents?)?.isError == true
+
+        val scheduler = context.scheduler
+        val jobKey = context.jobDetail.key
 
         if (threwException || reportedError) {
             consecutiveFailureCount++
@@ -27,10 +41,7 @@ class JobFailureListener : JobListenerSupport() {
                 consecutiveFailureCount = 0
                 pauseCount++
 
-                val pauseDuration: Int = (2.0.pow(pauseCount.toDouble()).toInt() * 30).coerceAtMost(900)
-
-                val scheduler = context.scheduler
-                val jobKey = context.jobDetail.key
+                val pauseDuration: Int = (PAUSE_ESCALATION.pow(pauseCount.toDouble().coerceAtMost(MAX_PAUSE_COUNT)) * 30).toInt()
 
                 log.warn(
                     "Pausing execution of job {} for {} seconds due to consecutive failure count exceeding {}",
@@ -51,6 +62,13 @@ class JobFailureListener : JobListenerSupport() {
             }
         } else {
             consecutiveFailureCount = 0
+
+            if (lastFailure != null) {
+                if (Duration.between(lastFailure, Instant.now()).abs().toKotlinDuration() >= RESET_PAUSE_AFTER) {
+                    log.info("Resetting pause time for job {} as the last failure was at least {} ago", jobKey, RESET_PAUSE_AFTER)
+                    pauseCount = 0
+                }
+            }
         }
     }
 }
